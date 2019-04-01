c1e11062abbca8376a96ade1eaf1b8cff36828cb
LChoiSH <csh42504@google.com>
2019-04-01 14:42:38 +0900

    Issue#20 make rspone json format

--- backend/router/v1/findRoute.js
+++ backend/router/v1/findRoute.js
@@ -20,7 +20,7 @@ module.exports = (passport) => {
     startX = req.query.startX;
     startY = req.query.startY;
 
-    for (var i = 0; i < direction.data.length; i++) {
+    for (let i = 0; i < direction.data.length; i++) {
       endX = direction.data[i].loc.longitude;
       endY = direction.data[i].loc.latitude;
       output[direction.data[i].name] = await subway.shortestPath(startX, startY, endX, endY);
@@ -41,5 +41,53 @@ module.exports = (passport) => {
     }
     res.json(travelInfo);
   });
+
+  router.post('/findLoc', async (req, res) => {
+    let output = {
+      'areas' : []
+    };
+    let middle = {
+      'lat' : 0,
+      'lng' : 0
+    };
+
+    // 중간위치 계산
+    for (let i = 0; i < req.body.startLocs.length ; i++) {
+      middle.lat += req.body.startLocs[i].location.lat;
+      middle.lng += req.body.startLocs[i].location.lng;
+    }
+    middle.lat /= req.body.startLocs.length;
+    middle.lng /= req.body.startLocs.length;
+
+    // TODO : database와 연결해서 중간위치로 hotplace 불러옴
+    let hotplace = direction.data;
+
+    for (let j = 0; j < hotplace.length; j++) {
+      let area = {};
+      area['name'] = hotplace[j].name;
+      area['location'] = hotplace[j].loc;
+
+      // user들 각각 계산
+      let users = [];
+      for (let k = 0; k < req.body.startLocs.length ; k++) {
+        let user;
+        if ( req.body.startLocs[k].transportation == 'public') {
+          user = await subway.shortestPath(req.body.startLocs[k].location.lng, req.body.startLocs[k].location.lat, hotplace[j].loc.longitude, hotplace[j].loc.latitude);
+        } else {
+          user = await driving.shortestPath(req.body.startLocs[k].location.lng, req.body.startLocs[k].location.lat, hotplace[j].loc.longitude, hotplace[j].loc.latitude);
+        }
+
+        users.push(user);
+      }
+
+      area['users'] = users;
+      output.areas.push(area);
+
+    }
+
+    res.json(output);
+
+  });
+
   return router;
 };
