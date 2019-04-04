51b21a43ad078fc4d8bfbfa48fc040f2e2c442c1
chickengak <peyoumonephu@gmail.com>
2019-04-04 15:34:22 +0900

    Issue#27-bug-fix-on-main-webpage-about-API

---
+++ src/frontend/config/Api_template.js
@@ -0,0 +1,2 @@
+document.body.appendChild(document.createElement('script')).src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initializeMap";
+//Put your Api-key in YOUR_API_KEY
