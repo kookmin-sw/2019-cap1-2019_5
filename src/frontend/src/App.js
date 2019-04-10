f0f1ea17bda4570cdea7e06c69c4bffc8bdfb39b
20112909 <aaaa@kookmin.ac.kr>
2019-04-10 15:25:22 +0900

    install the package
    fix some error of 'npm start'

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -124,6 +124,7 @@ class MainTable extends React.Component {
   render() {
     const { classes } = this.props;
     return (
+
       <div className={classes.root}>
         <Table className={classes.table}>
           <TableBody height = '100%'>
