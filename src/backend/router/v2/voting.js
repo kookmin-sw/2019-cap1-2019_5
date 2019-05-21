72653d60f574859457253f618484bd9c3a4c4faa
ShinYoungHwan <show2881@gmail.com>
2019-05-21 13:51:38 +0900

    voting function using token

--- src/backend/router/v2/voting.js
+++ src/backend/router/v2/voting.js
@@ -2,20 +2,16 @@ const db = require('../../models')
 const express = require('express');
 const router = express.Router();
 
-
-
 module.exports = () => {
   router.use((req, res, next) => {
     next();
   });
   
   router.post('/makeVoting', async (req, res) => {
-    // let clientToken = req.query.token; //토큰으로 받을경우
-    // let locIndex = req.query.index;
-    // let meeting = await db.Meeting.findOne({token : clientToken})
-    let votingResult = new Array();
-    let locIndex = 0;
-    let meeting = await db.Meeting.findOne({name : "테스트"})
+    let clientToken = req.query.token;
+    let locIndex = req.body.index;
+    let meeting = await db.Meeting.findOne({token : clientToken})
+
     let resultData = await db.Result.findOne({meetingID : meeting._id})
     let resultid = resultData._id;
 
@@ -23,15 +19,12 @@ module.exports = () => {
       ResultID: resultid,
       location: resultData.areas[locIndex].name
     });
-
-    await votingData.save();
     
-    for(let i = 0; i < resultData.areas.length ; i++)
-    {
-      votingResult[i] = await db.VotedLocs.find({ResultID : resultid,location : resultData.areas[i].name}).count();
-    }
-
-    res.send(votingResult)
+    let resultValue;
+    await votingData.save().then(result => {
+      resultValue = result;
+    });
+    res.send(resultValue);
   });
 
   return router;
